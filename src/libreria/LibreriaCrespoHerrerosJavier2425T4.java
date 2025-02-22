30057895cb804bfe140cec13ff413c386ded2fb0
JAVI <JAVI@10.0.2.15>
2025-02-22 14:02:50 +0100

    Mi proyecto CrespoHerrerosJavier2425T4 comentado con javadoc

--- src/libreria/LibreriaCrespoHerrerosJavier2425T4.java
+++ src/libreria/LibreriaCrespoHerrerosJavier2425T4.java
@@ -1,43 +1,93 @@
 
 package libreria;
 
+/**
+ * Clase que representa una librería, donde se pueden gestionar libros, 
+ * realizar compras y gestionar el saldo disponible.
+ * 
+ * La librería tiene un libro con nombre, cantidad en stock, precio y un saldo 
+ * que se puede actualizar con ingresos o compras.
+ * 
+ * @author JAVI
+ */
 public class LibreriaCrespoHerrerosJavier2425T4 {
-    private String nom;
-    private int cantidad;
+    private String nom;   
+    private int cantidad; 
     private double precio;
     private double saldo;
+    
     //Constructor sin argumentos
     public LibreriaCrespoHerrerosJavier2425T4(){}
-    //constructor con parámetros para inicializar todas las propiedades de la clase
+    
+     /**
+     * Constructor con parámetros para inicializar todas las propiedades de la librería (clase).
+     * 
+     * @param nom Nombre del libro.
+     * @param cantidad Cantidad de ejemplares disponibles en stock.
+     * @param precio Precio unitario del libro.
+     * @param saldo Saldo disponible en la cuenta de la librería.
+     */
     public LibreriaCrespoHerrerosJavier2425T4(String nom, int cantidad, double precio, double saldo){
         this.nom = nom;                 //Nombre del libro
         this.cantidad = cantidad;       //Cantidad de ejemplares que hay en stock
         this.precio = precio;           //Precio unitario del libro
         this.saldo = saldo;             //Saldo en la cuenta de la librería
     }
-    //Método que devuelve el nombre del libro
+    
+     /**
+     * Método que devuelve el nombre del libro.
+     * 
+     * @return El nombre del libro.
+     */
     public String obtenerNombre(){
         return getNom();
     }
-    //Método que devuelve el precio del libro
+    
+    /**
+     * Método que devuelve el precio del libro.
+     * 
+     * @return El precio del libro.
+     */
     public double obtenerPrecio(){
         return getPrecio();
     }
-    //Método que devuelve la cantidad de libros de ese ejemplar que quedan para vender
+    
+    /**
+     * Método que devuelve la cantidad de libros disponibles en stock (disponibles para vender).
+     * 
+     * @return La cantidad de libros disponibles.
+     */
     public int obtenerStock(){
         return getCantidad();
     }
-    //Método para obtener el saldo en la cuenta
+    
+    /**
+     * Método que devuelve el saldo disponible en la cuenta de la librería.
+     * 
+     * @return El saldo actual en la cuenta de la librería.
+     */
     public double obtenerSaldo(){
         return getSaldo();
     }
-    //Método para aumentar el saldo
+    
+    /**
+     * Método para aumentar el saldo disponible en la cuenta de la librería.
+     * 
+     * @param saldomas Cantidad de dinero a ingresar en el saldo de la librería.
+     * @throws Exception Si la cantidad a ingresar es menor o igual a cero, se lanza una excepción.
+     */
     public void aumentarSaldo(double saldomas) throws Exception{
         if (saldomas<=0)
             throw new Exception("No se puede ingresar 0 o una cantidad negativa");
         setSaldo(getSaldo() + saldomas);
     }
-    //Método para comprar un ejemplar de un libro, actualiza el stock y el saldo
+    
+    /**
+     * Método para comprar un libro de la librería, actualizando el stock y el saldo de la librería.
+     * 
+     * @param canti Cantidad de ejemplares a comprar.
+     * @throws Exception Si hay un error con la cantidad de libros, el saldo o el stock.
+     */
     public void comprarLibro(int canti) throws Exception{
         if (canti <= 0)
             throw new Exception ("No se puede retirar una cantidad negativa de libros");
@@ -54,57 +104,77 @@ public class LibreriaCrespoHerrerosJavier2425T4 {
        
     }
 
+   // Métodos getter y setter
+
     /**
-     * @return the nom
+     * Obtiene el nombre del libro.
+     * 
+     * @return El nombre del libro.
      */
     public String getNom() {
         return nom;
     }
 
-    /**
-     * @param nom the nom to set
+     /**
+     * Establece el nombre del libro.
+     * 
+     * @param nom El nombre del libro.
      */
     public void setNom(String nom) {
         this.nom = nom;
     }
 
-    /**
-     * @return the cantidad
+   /**
+     * Obtiene la cantidad de libros disponibles en stock.
+     * 
+     * @return La cantidad de libros disponibles.
      */
     public int getCantidad() {
         return cantidad;
     }
 
-    /**
-     * @param cantidad the cantidad to set
+     /**
+     * Establece la cantidad de libros disponibles en stock.
+     * 
+     * @param cantidad La cantidad de libros disponibles.
      */
     public void setCantidad(int cantidad) {
         this.cantidad = cantidad;
     }
 
+  
     /**
-     * @return the precio
+     * Obtiene el precio unitario del libro.
+     * 
+     * @return El precio del libro.
      */
     public double getPrecio() {
         return precio;
     }
 
-    /**
-     * @param precio the precio to set
+   /**
+     * Establece el precio unitario del libro.
+     * 
+     * @param precio El precio del libro.
      */
     public void setPrecio(double precio) {
         this.precio = precio;
     }
 
     /**
-     * @return the saldo
+     * Obtiene el saldo disponible en la cuenta de la librería.
+     * 
+     * @return El saldo disponible.
      */
     public double getSaldo() {
         return saldo;
     }
 
+   
     /**
-     * @param saldo the saldo to set
+     * Establece el saldo disponible en la cuenta de la librería.
+     * 
+     * @param saldo El saldo disponible.
      */
     public void setSaldo(double saldo) {
         this.saldo = saldo;
--- src/libreria/Main.java
+++ src/libreria/Main.java
@@ -1,9 +1,20 @@
 
 package libreria;
-
+/**
+ * Clase principal del programa en la que se simula las acciones de compra, consulta de stock
+ * y recarga de fondos para realizar la compra de un libro en una librería
+ * @author JAVI
+ */
 public class Main {
-  
+  /**
+   * Método Main
+   * 
+   * Método principal de ejecución del programa
+   *  
+   */
     public static void main(String[] args) {
+       
+        // Declaración de variables necesarias
         LibreriaCrespoHerrerosJavier2425T4 miLibreriaCrespoHerrerosJavier2425T4;
         String x;
         int stock;
@@ -12,50 +23,94 @@ public class Main {
         double saldoActual;
         double ingreso;
         
+        // Instanciamos un objeto de la clase LibreriaCrespoHerrerosJavier2425T4
         miLibreriaCrespoHerrerosJavier2425T4 = new LibreriaCrespoHerrerosJavier2425T4("Don Quijote de la Mancha", 10, 45, 120);
-
+        
+        // Simulamos la compra del libro "Don quijote de la Mancha"
         compraQuijoteCrespoHerrerosJavier2425T4(miLibreriaCrespoHerrerosJavier2425T4);
         
+        // Mostramos el saldo actual tras comprar el libro, empleando para ello el método "obtenerSaldo()"
         saldoActual = miLibreriaCrespoHerrerosJavier2425T4.obtenerSaldo();
-        System.out.println("El saldo al comprar el libro es de "+ saldoActual );        
+        System.out.println("El saldo al comprar el libro es de "+ saldoActual );   
+        
+        // Consultamos el nombre del libro y el stock actual empleando para ello los métodos "obtenerNombre()" y "obtenerStock()" respectivamente
         x = miLibreriaCrespoHerrerosJavier2425T4.obtenerNombre();
         stock = miLibreriaCrespoHerrerosJavier2425T4.obtenerStock();
         System.out.println("El libro "+x+" tiene un stock de "+stock+" unidades ahora");
         
-        añadeIngresoCrespoHerrerosJavier2425T4(miLibreriaCrespoHerrerosJavier2425T4, "Libro");
+        // Mediante el método "añadeIngresoCrespoHerrerosJavier2425T4" añadimos ingresos de dinero en la librería
+        añadeIngresoCrespoHerrerosJavier2425T4(miLibreriaCrespoHerrerosJavier2425T4, "");
     }
-
+    
+    /**
+     * Método que simula un ingreso de dinero en la librería.
+     * Este método, mediante la estructura try-catch, aumenta el saldo de la librería por una cantidad especificada que sea válida y muestra por pantalla el saldo total disponible,
+     * de lo contrario, si el valor de la cantidad a introducir no es válida se ejecutará el mensaje de error (catch)
+     * 
+     * @param miLibreriaCrespoHerrerosJavier2425T4 La instancia de la librería.
+     * @param concepto El concepto o descripción del ingreso (en este caso será el valor por defecto).
+     */
     public static void añadeIngresoCrespoHerrerosJavier2425T4(LibreriaCrespoHerrerosJavier2425T4 miLibreriaCrespoHerrerosJavier2425T4, String concepto) {
+       
+        // Declaramos las variables necesarias
         double ingreso;
         double saldoActual;
         try
         {
-            ingreso = 30.5; //Ingreso que se va a realizar en el saldo de la librería
+            //Ingreso que se va a realizar en el saldo de la librería
+            ingreso = 30.5; 
+            
+            // Aumentamoos el saldo por el valor del parámetro saldomas, que en este caso es el valor almacenado en la variable "ingreso"
             miLibreriaCrespoHerrerosJavier2425T4.aumentarSaldo(ingreso);
+            
+            // Almacenamos en la variable saldoActual el valor del saldo total actual
             saldoActual = miLibreriaCrespoHerrerosJavier2425T4.obtenerSaldo();
+            
+            // Mostramos por pantalla el saldo total después del ingreso realizado
             System.out.println("Tu saldo actual después de ingresar es de "+saldoActual+"€");
         } catch (Exception e)
         {
+            // En caso de que la cantidad introducida no sea válida se obtendrá el emnsaje de error
             System.out.println("Fallo al obtener el saldo al ingresar");
         }
     }
-
+ /**
+     * Método que simula la compra de un libro.
+     * Este método realiza la compra de una cantidad específica de un libro,
+     * descontando el saldo correspondiente y actualizando el stock.
+     * 
+     * @param miLibreriaCrespoHerrerosJavier2425T4 La instancia de la librería.
+     */
     public static void compraQuijoteCrespoHerrerosJavier2425T4(LibreriaCrespoHerrerosJavier2425T4 miLibreriaCrespoHerrerosJavier2425T4) {
+       
+        // Declaramos las variables necesarias
         String x;
         int num;
         double dinero;
         double saldoActual;
         try
         {
+            // Título del libro a comprar
             x = "Don Quijote de la Mancha";
-            num = 2;    //Número de ejemplares a comprar
+            
+            //Número de ejemplares a comprar
+            num = 2;    
+            
+            // Almacenamos en la variable "dinero" el precio del libro
             dinero = miLibreriaCrespoHerrerosJavier2425T4.obtenerPrecio();
+            
+            // Almacenamos en la variable "saldoActual" el saldo total disponible
             saldoActual = miLibreriaCrespoHerrerosJavier2425T4.obtenerSaldo();
+            
+            // Mostramos por pantalla el libro y su precio, así como el saldo actual del que se dispone
             System.out.println("El libro "+x+" vale "+dinero+"€");
             System.out.println("El saldo actual de la librería es de "+saldoActual+"€");
+            
+            // Ejecutamos el método que permite comprar el libro con la cantidad de ejemplares a comprar (parámetro "num")
             miLibreriaCrespoHerrerosJavier2425T4.comprarLibro(num);
         } catch (Exception e)
         {
+            // En caso de que el el valor de libros a comprar o el saldo actual no sea el suficiente se ejecutará el mensaje de error
             System.out.println("Error en la compra del libro");
         }
     }
